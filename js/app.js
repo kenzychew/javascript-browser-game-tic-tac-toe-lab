aa59af308bea3bce9c2dfe09a2d506baf2dd4b0a
kenzychew <kenzychew@gmail.com>
2024-12-18 12:50:04 +0800

    feat: step 3

--- js/app.js
+++ js/app.js
@@ -4,22 +4,31 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board
-let turn
-let winner
-let tie
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
-const squareEls = document.querySelector(".sqr")
-const messageEl = document.querySelector("#message")
+const squareEls = document.querySelector(".sqr");
+const messageEl = document.querySelector("#message");
+
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-
+function init() {
+    board = ["", "", "", "", "", "", "", "", "",];
+    turn = "X";
+    winner = null;
+    tie = false;
+    render();
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
+// initialize game
+init();
